5dd61464f83c3e83e0d8e4c368aeeb0eb13045ba
Amit Upadhyay <amitu@cdac.in>
2022-12-09 13:32:48 +0530

    template

--- learn-spring-framework/src/main/java/com/amitu/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
+++ learn-spring-framework/src/main/java/com/amitu/learnspringframework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -1,18 +1,23 @@
 package com.amitu.learnspringframework.examples.a0;
 
+import java.util.Arrays;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.amitu.learnspringframework.examples.a0")
+@ComponentScan
 public class SimpleSpringContextLauncherApplication {
 	
 	public static void main(String[] args) {
 		
 		try (var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class)) {
-
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
 		} catch (BeansException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
